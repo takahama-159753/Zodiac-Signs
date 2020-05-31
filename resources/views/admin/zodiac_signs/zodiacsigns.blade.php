71efdf6bee166bb43ea128d2d8260cf0eafc8e82
”高浜 <takahama.sin.04130413@gmail.com>
2020-05-31 09:29:45 +0000

    20200531_DB作成中_星座の画面作成中_t4

--- resources/views/admin/zodiac_signs/zodiacsigns.blade.php
+++ resources/views/admin/zodiac_signs/zodiacsigns.blade.php
@@ -8,9 +8,15 @@
 {{-- main.blade.phpの@yield('content')に以下のタグを埋め込む --}}
 @section('content') 
 <div class="container">
+  <img src="{{ secure_asset('img/aries.jpg')}}" alt="background" class="background-img">
   <div class="row">
     <div class="col-md-8 mx-auto">
-      <img src="{{ secure_asset('img/image656-1280x800.jpg')}}" alt="background" class="background-img">
+      <p>おひつじ座は古い星座なので、ギリシア神話にもその物語が伝えられています。</p>
+      <p>その物語によると、おひつじ座のモデルは、金の毛をもつ、空飛ぶ牡羊と言われています。</p>
+      <p>この牡羊は、テッサリアの王・アタマスの息子・プリクソスと双子の妹・ヘレが、継母・イノーの企みによって生贄にされそうになったとき、</p>
+      <p>  ふたりを助け出したヒツジで、ヘルメスが大神・ゼウスから預けられていたヒツジとされています。</p>
+      <p>妹のヘレは途中で海に落ちてしまいますが、逃げ延びたプリクソスは、ヒツジが亡くなった後、その毛皮をコルキス王に贈り、国の宝にしたということです。</p>
+      <p>その後、このヒツジの皮を手に入れるための冒険が、アルゴ遠征隊の冒険へと繋がっていきますが、このふたりを助け出した牡羊が、おひつじ座のモデルになっています。</p>
     </div>
   </div>
 </div>
