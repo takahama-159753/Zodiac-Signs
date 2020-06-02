402ee59e3d056ab628bf90cb63a9b0056a644b68
”高浜 <takahama.sin.04130413@gmail.com>
2020-06-02 09:29:59 +0000

    20200602_星座画面12枚完成_ログイン画面作成中_web.phpにmiddleware('auth');を追加_ログイン画面調整中_t6

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,7 +5,6 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="login-box card">
-                    <div class="login-header card-header mx-auto">{{ __('Login') }}</div>
 
                     <div class="login-body card-body">
                         <form method="POST" action="{{ route('login') }}">
