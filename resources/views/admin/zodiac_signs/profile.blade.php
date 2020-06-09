8443f2cffa884918c4d20b96b4a23881bbe18a8c
”高浜 <takahama.sin.04130413@gmail.com>
2020-06-09 19:32:34 +0000

    20200609_星座画面12枚完成_ログイン画面作成中_web.phpにmiddleware('auth');を追加_ログイン画面調整中_t15

--- resources/views/admin/zodiac_signs/profile.blade.php
+++ resources/views/admin/zodiac_signs/profile.blade.php
@@ -11,7 +11,8 @@
     <div class="col-md-8 mx-auto">
       <img src="{{ secure_asset('img/image656-1280x800.jpg')}}" alt="background" class="background-img">
       <div style="width:300px; margin:0 auto;">
-      <p class="zodiactext">初めまして。高浜真之介と申します。<br>
+      <p class="zodiactext">
+         初めまして。高浜真之介と申します。<br>
          プログラミングに興味を持ち、techboostに入会しました。<br>
          上手くいかず、課題を行うのに時間が掛かってしまいました。<br>
          最近の趣味は、マジックザギャザリングです。<br>
