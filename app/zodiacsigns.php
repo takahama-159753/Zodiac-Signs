d759a53d6e5ada545b798d2cd840af003504805c
”高浜 <takahama.sin.04130413@gmail.com>
2020-06-09 13:10:36 +0000

    20200609_星座画面12枚完成_ログイン画面作成中_web.phpにmiddleware('auth');を追加_ログイン画面調整中_t12

--- app/zodiacsigns.php
+++ app/zodiacsigns.php
@@ -7,7 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class zodiacsigns extends Model
 {
     public static $rules = array(
-        'title' => 'required',
-        'body' => 'required',
     );
 }
--- resources/views/admin/zodiac_signs/birthday.blade.php
+++ resources/views/admin/zodiac_signs/birthday.blade.php
@@ -8,11 +8,15 @@
 {{-- main.blade.phpの@yield('content')に以下のタグを埋め込む --}}
 @section('content') 
 <div class="container">
-  <div class="row">
-    <div class="col-md-8 mx-auto">
+  <div class="row justify-content-center">
+    <div class="col-md-8">
+      <div class="form-group row">
      <img src="{{ secure_asset('img/image656-1280x800.jpg')}}" alt="background" class="background-img1">
-    <div style="box1"><input type="date" name="calendar" max="9999-12-31"></div>
-    <div style="box1"><a href="{{url('admin/zodiac_signs/zodiacsigns')}}">調べる</a></div>
+      <div class="box1">
+    <input type="date" name="calendar" max="9999-12-31">
+    <a href="{{url('admin/zodiac_signs/zodiacsigns')}}">調べる</a>
+      </div>
+     </div>
     </div>
   </div>
 </div>
