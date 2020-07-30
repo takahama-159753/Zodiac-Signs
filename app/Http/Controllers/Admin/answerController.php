15f344aa138a825ffbfaafec89ed4ccdf89658a3
”高浜 <takahama.sin.04130413@gmail.com>
2020-07-30 13:08:44 +0000

    20200730_t28

--- app/Http/Controllers/Admin/answerController.php
+++ app/Http/Controllers/Admin/answerController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
-class quizController extends Controller
+class answerController extends Controller
 {
   public function add()
   {
